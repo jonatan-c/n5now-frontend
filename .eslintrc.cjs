49b0dc502d52d55f97072aa8194a3cd239164250
jonatan-c <jona.cc.93@gmail.com>
2024-06-20 10:18:30 -0300

    feat: added team dev tools

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.js'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier' , 'eslint-plugin-import-helpers'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    "react/prop-types": "off",
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }], 
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        newlinesBetween: "always",
+        groups: [
+          ["/^next/", "module"],
+          "/@styles/",
+          "/@components/",
+          "/@lib/",
+          ["parent", "sibling", "index"],
+        ],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
+    ],
+  },
+}
+
